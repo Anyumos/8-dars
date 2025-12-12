fff04a015661d0315e929f3ce2e6b39079332a8b
QWERTY <baltabaykalandarov@gmail.com>
2025-12-12 17:44:59 +0500

    added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('WELCOME MY SITE');
+    res.status(200).json({
+    messega: 'Welcome to my server'
+    })
 });
 
 
