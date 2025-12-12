7c616e160f78f8113fb6fba88ac7a3444c56f879
QWERTY <baltabaykalandarov@gmail.com>
2025-12-12 17:34:56 +0500

    added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('WELCOME MY SITE');
+});
+
 
 // Router
 app.use(authRouter)
